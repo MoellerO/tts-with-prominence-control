91d118d631609ca03ba3b3bfdd0a10aa1dc2f346
MoellerO <oliver.moeller@die-olchies.de>
2023-03-17 18:08:44 +0100

    Code Oliver

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan, set_seed
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 import torch
+import soundfile as sf
 
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
 vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
 inputs = processor(text="Hello, my dog is cute", return_tensors="pt")
-speaker_embeddings = torch.zeros((1, 512))  # or load xvectors from a file
 
-set_seed(555)  # make deterministic
+# load xvector containing speaker's voice characteristics from a dataset
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
-# generate speech
-speech = model.generate_speech(
-    inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
 
-print(speech.shape)
+sf.write("speech.wav", speech.numpy(), samplerate=16000)
